6a4e414bcc3061338aba6813c68f5fdb43b19da8
Jacques Nithart <jacquesnithart2@gmail.com>
2026-06-19 09:05:43 +0200

    CPUs 2

--- submit.sh
+++ submit.sh
@@ -2,12 +2,12 @@
 #SBATCH -J Abaqus-
 #SBATCH -o log.out
 #SBATCH -p q36
-#SBATCH --nodes=1 --ntasks=1 --cpus-per-task=16
+#SBATCH --nodes=1 --ntasks=1 --cpus-per-task=20
 #SBATCH --mem=120000
 #SBATCH --time=00-02:00:00
 
 echo "======= Started at  `date` ======="
 echo
-subabqpy2025-old -p q36 -c 8 -m 120 -t 02:00:00 run_single
+subabqpy2025-old -p q36 -c 20 -m 120 -t 02:00:00 run_single
 echo
 echo "======= Finished at `date` ======="
