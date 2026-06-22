8ad4267eae6f5f4f8c6b93e2ec9249e7ced4b045
Jacques Nithart <jacquesnithart2@gmail.com>
2026-06-22 10:42:42 +0200

    Changes for ALE mesh tests

--- submit.sh
+++ submit.sh
@@ -2,6 +2,6 @@
 
 echo "======= Started at  `date` ======="
 echo
-subabqpy2025-old -p q36 -c 20 -m 120 -t 04:00:00 run_mesh_convergence
+subabqpy2025-old -p q36 -c 20 -m 120 -t 06:00:00 run_mesh_convergence
 echo
 echo "======= Finished at `date` ======="
