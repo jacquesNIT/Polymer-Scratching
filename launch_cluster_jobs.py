267f40565695b6c86209d3f2623e3a54c35bfa8c
Jacques Nithart <jacquesnithart2@gmail.com>
2026-07-09 11:25:06 +0200

    Test

--- launch_cluster_jobs.py
+++ launch_cluster_jobs.py
@@ -17,10 +17,10 @@ import sys
 # NB : make sure that the number of jobs * CPUs per job < CPUs of the node 
 JOBS = [
     ("mesh", "elastomer_mr"),       
-    ("mesh", "glassy_pmma"),
-    ("mesh", "glassy_dp"),       
-    ("mesh", "semicrystalline_j2"),
-    ("mass_scale", "glassy_pc"),
+    #("mesh", "glassy_pmma"),
+    #("mesh", "glassy_dp"),       
+    #("mesh", "semicrystalline_j2"),
+    #("mass_scale", "glassy_pc"),
     # ("models", "elastomer_mr"),
 ]
 SWEEP_JOBS = 8                  # number of jobs for the "material" sweep 
