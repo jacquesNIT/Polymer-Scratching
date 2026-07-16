cf0a36c41e9d8ea6b65659675b5394a558cf79f3
Jacques Nithart <jacquesnithart2@gmail.com>
2026-07-16 10:10:12 +0200

    Test

--- submit.sh
+++ submit.sh
@@ -2,6 +2,6 @@
 
 echo "======= Started at  `date` ======="
 echo
-subabqpy2025-old -p q32g -c 16 -m 100 -t 10:00:00 run_parameter_study 
+subabqpy2025-old -p q128 -c 18 -m 100 -t 10:00:00 run_parameter_study 
 echo
 echo "======= Finished at `date` ======="
