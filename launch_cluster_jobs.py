f2ff815fb24fe9804320e598923f4da3149a57be
Jacques Nithart <jacquesnithart2@gmail.com>
2026-07-24 10:54:06 +0200

    Tests

--- launch_cluster_jobs.py
+++ launch_cluster_jobs.py
@@ -59,12 +59,12 @@ JOBS = [
 
     #("single", "glassy_pc", {"tag": "Test_NewMeshing_z10", "ALE": True, "scratch_time": 0.05, "z_size": 0.01}),
     #("single", "glassy_pc", {"tag": "Test_NewMeshing_z15", "ALE": True, "scratch_time": 0.05, "z_size": 0.015}),
-    ("single", "glassy_pc", {"tag": "Test", "ALE": True, "scratch_time": 0.001}),
+    #("single", "glassy_pc", {"tag": "Test", "ALE": True, "scratch_time": 0.001}),
     #("single", "glassy_pc", {"tag": "Test_NewMeshing_z30", "ALE": True, "scratch_time": 0.05, "z_size": 0.030}),
 
-    #("mesh", "glassy_pc", {"tag": "distortionF1", "ALE": False, "scratch_time": 0.05, "distortion": True, "length": 0.1}),
-    #("mesh", "glassy_pc", {"tag": "distortionF2", "ALE": False, "scratch_time": 0.05, "distortion": True, "length": 0.2}),
-    #("mesh", "glassy_pc", {"tag": "distortionF3", "ALE": False, "scratch_time": 0.05, "distortion": True, "length": 0.3}),
+    ("mesh", "glassy_pc", {"tag": "distortion_base", "ALE": False, "scratch_time": 0.05, "distortion": False, "length": 0.1}),
+    #("mesh", "glassy_pc", {"tag": "distortionF2", "ALE": False, "scratch_time": 0.05, "distortion": False, "length": 0.2}),
+    #("mesh", "glassy_pc", {"tag": "distortionF3", "ALE": False, "scratch_time": 0.05, "distortion": False, "length": 0.3}),
     #("mesh", "glassy_pc", {"tag": "distortionT1", "ALE": True, "scratch_time": 0.05, "distortion": True, "length": 0.1}),
     #("mesh", "glassy_pc", {"tag": "distortionT2", "ALE": True, "scratch_time": 0.05, "distortion": True, "length": 0.2}),
     #("mesh", "glassy_pc", {"tag": "distortionT3", "ALE": True, "scratch_time": 0.05, "distortion": True, "length": 0.3}),
