77d808385117bf15d00ac54fe69fd6cee427a13d
Jacques Nithart <jacquesnithart2@gmail.com>
2026-08-06 15:19:10 +0200

    Tests

--- AbaqusModel/Configuration/base.py
+++ AbaqusModel/Configuration/base.py
@@ -789,8 +789,8 @@ class Simulation_Config:
                 fine_size_y=0.03,
                 fine_size_z=0.03,    
                 coarse_size_0=0.02,     # Unused
-                coarse_size_1=0.06,     # 0.07*4 
-                coarse_size_2=0.12,     # 0.07*8
+                coarse_size_1=0.028,     # 0.07*4 
+                coarse_size_2=0.56,     # 0.07*8
                 hourglass_control="ENHANCED",      # RELAX STIFFNESS with ALE / ENHANCED without ALE 
                 distortion_control="DEFAULT",
                 max_degradation=0.9,
