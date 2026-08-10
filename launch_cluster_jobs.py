64d87a9e529efce09a7763ddd1d3404485d968bc
Jacques Nithart <jacquesnithart2@gmail.com>
2026-08-10 11:02:54 +0200

    test

--- launch_cluster_jobs.py
+++ launch_cluster_jobs.py
@@ -12,11 +12,11 @@ JOBS = [
     #("mesh", "glassy_pc", {"tag": "mesh50", "ALE": False, "scratch_time": 0.05, "distortion": True, "length": 0.1,"unload_time": 0.05,"recovery_time": 0.05}),
     #("mesh", "glassy_pc", {"tag": "mesh25", "ALE": False, "scratch_time": 0.025, "distortion": True, "length": 0.1,"unload_time": 0.05,"recovery_time": 0.05}),
     #("mesh", "glassy_pc", {"tag": "mesh10", "ALE": False, "scratch_time": 0.01, "distortion": True, "length": 0.1,"unload_time": 0.05,"recovery_time": 0.05}),
-    #("mesh", "glassy_pc", {"tag": "mesh5", "ALE": False, "scratch_time": 0.005, "distortion": True, "length": 0.1,"unload_time": 0.05,"recovery_time": 0.05}),
+    ("mesh", "glassy_pc", {"tag": "mesh5bis", "ALE": False, "scratch_time": 0.005, "distortion": True, "length": 0.1,"unload_time": 0.05,"recovery_time": 0.05}),
 
     #("mass_scale", "glassy_pmma", {"tag": "MS001bis", "ALE": False, "scratch_time": 0.01, "distortion": True, "length": 0.1,"unload_time": 0.05,"recovery_time": 0.05}),
 
-    ("single", "glassy_pmma", {"tag": "quick_test0", "ALE": False, "scratch_time": 0.01,"unload_time": 0.005,"recovery_time": 0.005, "distortion": True, "length": 0.1}),
+    #("single", "glassy_pmma", {"tag": "quick_test0", "ALE": False, "scratch_time": 0.01,"unload_time": 0.005,"recovery_time": 0.005, "distortion": True, "length": 0.1}),
     #("single", "glassy_pmma", {"tag": "Verif25", "ALE": False, "scratch_time": 0.025, "distortion": True, "length": 0.1}),
     #("single", "glassy_pmma", {"tag": "Verif50", "ALE": False, "scratch_time": 0.05, "distortion": True, "length": 0.1}),
 
