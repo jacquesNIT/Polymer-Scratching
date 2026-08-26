36917f71b8e14bce22e697438e4e14baeacc2345
Jacques Nithart <jacquesnithart2@gmail.com>
2026-08-26 13:00:11 +0200

    Tests

--- launch_cluster_jobs.py
+++ launch_cluster_jobs.py
@@ -10,7 +10,8 @@ JOBS = [
 
 
     # To launch on 0.01 mesh with freq=20 ALE (~2-4 days)
-    ("design", "glassy_pc", {"tag": "Morris6", "ALE": True, "scratch_time": 0.01, "distortion": False,"unload_time": 0.005,"recovery_time": 0.005}), 
+    ("design", "glassy_pc", {"tag": "Morris7", "ALE": True, "scratch_time": 0.01, "distortion": False,"unload_time": 0.005,"recovery_time": 0.005}), 
+    
     #("design", "glassy_pc", {"tag": "Morris4_001", "ALE": True, "scratch_time": 0.01, "distortion": False,"unload_time": 0.005,"recovery_time": 0.005}), 
 
 
