02fee92a30e09257b4fb9afd7dbaad3b793156f5
Jacques Nithart <jacquesnithart2@gmail.com>
2026-09-07 11:34:21 +0200

    Tests

--- AbaqusModel/Configuration/families.py
+++ AbaqusModel/Configuration/families.py
@@ -188,13 +188,13 @@ def _glassy_pmma_config():
         rho=1.19e-9,
         hyperelastic=LinearElastic_Config(E=3300.0, nu=0.37),
         plasticity=DruckerPrager_Config(
-            friction_angle=25.0, flow_stress_ratio=1.0, dilation_angle=5.0,      # dilation_angle = 10 originally, 5 or 0 for calibration tests
-            yield_table=gsell_jonas_table(sigma_y0=103.0, h=0.45,                # sigma_y0 = 103 originally, 160 for calibration tests
-                                          soft_drop=25.0, eps_soft=0.06,         # soft_drop = 25 orginally, 39 for calibration tests
+            friction_angle=25.0, flow_stress_ratio=1.0, dilation_angle=10.0,      # dilation_angle = 10 originally, 5 or 0 for calibration tests
+            yield_table=gsell_jonas_table(sigma_y0=160.0, h=0.45,                # sigma_y0 = 103 originally, 160 for calibration tests
+                                          soft_drop=39.0, eps_soft=0.06,         # soft_drop = 25 orginally, 39 for calibration tests
                                           eps_max=2.0, n_points=60),
             rate_dependent=None),
             #rate_dependent=RateDependent_Config.from_eyring(sigma_y0=105.0, S_per_decade=9.0)),
-        friction=Friction_Config.briscoe(tau0=40.0, alpha=0.05),                   # Originally (4,0.2), tested (25,0.1) and (40,0.05) for calibration                                         # Plausible value for tau0 and alpha, to be determined
+        friction=Friction_Config.briscoe(tau0=4.0, alpha=0.2),                   # Originally (4,0.2), tested (25,0.1) and (40,0.05) for calibration                                         # Plausible value for tau0 and alpha, to be determined
         #friction=Friction_Config(mu=0.3),
         family="glassy_pmma",
     )
