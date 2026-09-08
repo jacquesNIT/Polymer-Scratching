53bc369768c0e7827c38222798977e0711460b76
Jacques Nithart <jacquesnithart2@gmail.com>
2026-09-08 08:57:10 +0200

    Tests

--- submit.sh
+++ submit.sh
@@ -2,6 +2,6 @@
 
 echo "======= Started at  `date` ======="
 echo
-/home/bin/subabqpy_mine -p q36 -c 6 -m 100 -t 4-00:00:00 run_parameter_study
+/home/au824386/bin/subabqpy_mine -p q36 -c 6 -m 100 -t 4-00:00:00 run_parameter_study
 echo
 echo "======= Finished at `date` ======="
